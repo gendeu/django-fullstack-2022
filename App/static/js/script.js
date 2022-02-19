756eb57e5c5d4c310f08df61d8fcb167c6f11746
gendeu03 <gendeulgagarin@gmail.com>
2022-02-19 23:46:14 +0800

    added patient filter, add fomr

--- App/static/js/script.js
+++ App/static/js/script.js
@@ -36,4 +36,25 @@ $(document).ready(function(){
         }
        //Code 
      });
+
+     $('[data-toggle="tooltip"]').tooltip();
+	
+	// Select/Deselect checkboxes
+	var checkbox = $('table tbody input[type="checkbox"]');
+	$("#selectAll").click(function(){
+		if(this.checked){
+			checkbox.each(function(){
+				this.checked = true;                        
+			});
+		} else{
+			checkbox.each(function(){
+				this.checked = false;                        
+			});
+		} 
+	});
+	checkbox.click(function(){
+		if(!this.checked){
+			$("#selectAll").prop("checked", false);
+		}
+	});
  });
--- App/views.py
+++ App/views.py
@@ -5,17 +5,34 @@ from django.contrib.auth.decorators import login_required
 from App.models import Patient
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Function to render frontend
 def frontend(request):
     return render(request, "frontend.html")
 
 # -------------  Backend Section  ----------------
-# Function to render frontend
+# Function to render backend
 @login_required(login_url="login")
 def backend(request):
-    return render(request, "backend.html")
+    if 'q' in request.GET:
+        q = request.GET['q']
+        all_patient_list = Patient.objects.filter (
+            Q(firstname__icontains=q) | Q(lastname__icontains=q) | Q(email__icontains=q) | Q(age__icontains=q) | Q(gender__icontains=q) | Q(note__icontains=q)
+        ).order_by('-created_at')   
+    else:
+        all_patient_list = Patient.objects.all().order_by('-created_at')
+    paginator = Paginator(all_patient_list, 5)
+    page = request.GET.get('page')
+    filtered_patients = paginator.get_page(page)
 
+    return render(request, "backend.html", {"patients":filtered_patients,"all_patients":all_patient_list})
+
+# @login_required(login_url="login")
+# def backend(request):
+#     return render(request, "backend.html")
+    
 # Function to insert new patient
 @login_required(login_url="login")
 def add_patient(request):
@@ -32,8 +49,8 @@ def add_patient(request):
             patient.save()
             messages.success(request, "Patient added successfully!")
             return HttpResponseRedirect('/backend')
-        # else:
-        #     messages.error(request, "Failed!")
-            # return HttpResponseRedirect('/backend')
+        else:
+            messages.error(request, "Failed!")
+            return HttpResponseRedirect('/backend')
     else:
-        return render(request, "add_patient.html")
+        return render(request, "backend.html")
