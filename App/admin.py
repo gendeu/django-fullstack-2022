53852d2e19d9bfb9dbaa64d7d9d56657aa69d764
Deo <klpdeo03@gmail.com>
2022-02-19 15:14:29 +0800

    added jazzmin for adminpage

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from App.models import Patient
 
 # Register your models here.
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['name','phone','email','gender','created_at']
+    search_fields = ['name','phone','email','gender']
+    list_per_page = 10
+
+admin.site.register(Patient, PatientAdmin)
